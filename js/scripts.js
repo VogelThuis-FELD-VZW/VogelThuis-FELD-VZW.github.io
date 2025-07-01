4d02115574fd4375fcc92c9076f971919f155905
VogelThuis-FELD-VZW <Info@VogelThuis-FELD.be>
2025-07-01 14:23:22 +0200

    Added GDPR Compliant cookie banner

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,20 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+document.addEventListener('DOMContentLoaded', function () {
+  const banner = document.getElementById('cookieConsentBanner');
+  const acceptBtn = document.getElementById('acceptCookies');
+
+  if (banner && acceptBtn) {
+    if (!localStorage.getItem('infoBannerDismissed')) {
+      banner.style.display = 'block';
+    } else {
+      banner.style.display = 'none';
+    }
+
+    acceptBtn.addEventListener('click', function () {
+      localStorage.setItem('infoBannerDismissed', 'true');
+      banner.style.display = 'none';
+    });
+  }
+});
